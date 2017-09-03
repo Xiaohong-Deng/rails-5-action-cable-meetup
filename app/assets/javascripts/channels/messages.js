c89371508f2c4aa7c09f0d526a3f056083d4e5ad
nicenick <worldsky86rough@gmail.com>
2017-09-03 22:18:16 +0800

    Add msg sending feat. to client side js and channel

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,9 +1,22 @@
-App.messages = App.cable.subscriptions.create('MessagesChannel', {  
+App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
-    $("#messages").removeClass('hidden')
-    return $('#messages').append(this.renderMessage(data));
-  },
-  renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    $("#messages").removeClass('hidden');
+    return $("[data-chatroom='" + data.chatroom_id + "']").append(data.message);
   }
 });
+
+$(document).on('turbolinks:load', function() {
+  submitNewMessage();
+});
+
+function submitNewMessage(){
+  $('textarea#message_content').keydown(function(event) {
+    if (event.keyCode === 13) {
+        var msg = event.target.value;
+        var chatroomId = $("[data-chatroom]").data().chatroom;
+        App.messages.send({message: msg, chatroom_id: chatroomId});
+        $('[data-textarea="message"]').val(" ");
+        return false;
+     }
+  });
+}
--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,5 +1,10 @@
-class MessagesChannel < ApplicationCable::Channel  
+class MessagesChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'messages'
   end
-end  
+
+  def receive(payload)
+    message = Message.create(chatroom_id: payload["chatroom_id", content: payload["message"]])
+    ActionCable.server.broadcast("messages", {message: message.content, chatroom_id: message.chatroom_id})
+  end
+end
